c45c1a119717c29f4dbc618d7d58d051b13a8996
AbdievMarat <abdiev.m.t@gmail.com>
2024-10-24 17:20:06 +0600

    refactor: detail remainder

--- resources/views/admin/orders/remainder_details.blade.php
+++ resources/views/admin/orders/remainder_details.blade.php
@@ -1,34 +1,41 @@
 <table class="table">
     <thead>
     <tr>
-        <th scope="col" colspan="3" class="text-center">Список остатков на {{ date('d.m.Y', strtotime($remainder->date)) }} от буса № {{ $remainder->bus->license_plate }}</th>
+        <th scope="col" colspan="4" class="text-center">
+            Список остатков на {{ date('d.m.Y', strtotime($remainder->date)) }} от буса № {{ $remainder->bus->license_plate }} <br>
+            Создано: {{ date('d.m.Y H:i', strtotime($remainder->created_at)) }}
+        </th>
     </tr>
     <tr>
-        <th scope="col" style="width: 50%">Продукт</th>
-        <th scope="col" style="width: 25%">Кол-во</th>
-        <th scope="col" style="width: 25%">Цена</th>
+        <th scope="col" style="width: 55%">Продукт</th>
+        <th scope="col" style="width: 15%">Кол-во</th>
+        <th scope="col" style="width: 15%">Цена</th>
+        <th scope="col" style="width: 15%">Сумма</th>
     </tr>
     </thead>
     <tbody>
     @php
+        $totalSum = 0;
         $totalAmount = 0;
     @endphp
     @foreach($remainder->items as $item)
         @if($item->amount > 0)
             @php
-                $totalAmount += $item->amount * $item->price;
+                $totalSum = $item->amount * $item->price;
+                $totalAmount += $totalSum;
             @endphp
             <tr>
                 <td>{{ $item->product->name }}</td>
                 <td>{{ $item->amount }}</td>
                 <td>{{ $item->price }}</td>
+                <td>{{ $totalSum }}</td>
             </tr>
         @endif
     @endforeach
     </tbody>
     <tfoot>
     <tr>
-        <th colspan="2">Итого:</th>
+        <th colspan="3">Итого:</th>
         <th>{{ $totalAmount }}</th>
     </tr>
     </tfoot>
