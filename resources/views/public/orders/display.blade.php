781ef50257a1c6d19f137d05362de225e3442cb9
m.abdiev <abdiev.m.t@gmail.com>
2026-01-24 16:07:38 +0600

    style: display orders

--- resources/views/public/orders/display.blade.php
+++ resources/views/public/orders/display.blade.php
@@ -9,38 +9,34 @@
         </div>
 
         <div class="row">
-            <div class="col-12">
-                <div class="table-container">
-                    <table id="public-orders-table" class="table table-bordered table-hover">
-                        <thead>
-                        <tr>
-                            <th class="text-center align-middle" style="width: 40px;">Автобус</th>
-                            @foreach ($products as $product)
-                                <th class="vertical-text">{{ $product->name }}</th>
-                            @endforeach
-                        </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="table-info fw-bold" id="total-carts-row">
-                                <td class="text-center align-middle fw-bold">Тележки</td>
-                                @foreach ($totalCarts as $cartsCount)
-                                    <td class="text-center align-middle total-cart-cell">{{ $cartsCount }}</td>
-                                @endforeach
-                            </tr>
-                        @foreach ($busesData as $bus)
-                            <tr>
-                                <td class="text-center align-middle fw-bold bg-light">{{ $bus['license_plate'] }}</td>
-                                @foreach ($bus['products'] as $productData)
-                                    <td class="text-center align-middle">
-                                        {{ $productData['order_amount'] ?: '' }}
-                                    </td>
-                                @endforeach
-                            </tr>
+            <table id="public-orders-table" class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                    <th class="text-center align-middle" style="width: 40px;">Автобус</th>
+                    @foreach ($products as $product)
+                        <th class="vertical-text">{{ $product->name }}</th>
+                    @endforeach
+                </tr>
+                </thead>
+                <tbody>
+                <tr class="table-info fw-bold" id="total-carts-row">
+                    <td class="text-center align-middle fw-bold">Тележки</td>
+                    @foreach ($totalCarts as $cartsCount)
+                        <td class="text-center align-middle total-cart-cell">{{ $cartsCount }}</td>
+                    @endforeach
+                </tr>
+                @foreach ($busesData as $bus)
+                    <tr>
+                        <td class="text-center align-middle fw-bold bg-light">{{ $bus['license_plate'] }}</td>
+                        @foreach ($bus['products'] as $productData)
+                            <td class="text-center align-middle">
+                                {{ $productData['order_amount'] ?: '' }}
+                            </td>
                         @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 
